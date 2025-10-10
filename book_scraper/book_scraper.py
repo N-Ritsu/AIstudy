b55e39f89db8540b45fc7a9c97f7edce41f83e8a
N-Ritsu <rituki1018@gmail.com>
2025-10-10 17:48:43 +0900

    docs: 型ヒントとdocstringを追加

--- book_scraper/book_scraper.py
+++ book_scraper/book_scraper.py
@@ -4,35 +4,59 @@ import time
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin # URLを安全に連結するためのライブラリ
 
-def fetch_page(url):
-   try:
-      # URLにアクセスしてHTMLを取得する
-      response = requests.get(url)
-      # リンクが見つからなかった際、tryに引っかかるようにエラーを発生させる
-      response.raise_for_status()
-      response.encoding = response.apparent_encoding # 文字化け防止
-      return response.text
-   except requests.exceptions.RequestException as e:
-      print(f"URLへのアクセス中にエラーが発生しました：{e}")
-      return None
+def fetch_page(url: str) -> str | None:
+  """
+  受け取ったurlにアクセスし、そのページのHTMLを取得する
+  Args:
+    url (str): アクセス先のurl 
+  Returns:
+    str | None
+    - アクセス先のHTMLデータ
+    - アクセス中にエラーが発生した際の戻り値
+  """
+  try:
+    # URLにアクセスしてHTMLを取得する
+    response = requests.get(url)
+    # リンクが見つからなかった際、tryに引っかかるようにエラーを発生させる
+    response.raise_for_status()
+    response.encoding = response.apparent_encoding # 文字化け防止
+    return response.text
+  except requests.exceptions.RequestException as e:
+    print(f"URLへのアクセス中にエラーが発生しました：{e}")
+    return None
    
-def get_category_urls(html, base_url_for_join):
-    soup = BeautifulSoup(html, 'html.parser')
-    category_urls = []
-    # 'div.side_categoriesの中の、ulの中の、liの中の、aの中身' というCSSセレクタで目的のaタグをすべて取得
-    category_links = soup.select('div.side_categories ul li a')
-    
-    # 最初のリンクは 'Books' 全体なのでスキップ (スライス[1:]を使う)
-    for link in category_links[1:]:
-        # 相対パスを絶対パスに変換する
-        relative_url = link.get('href')
-        absolute_url = urljoin(base_url_for_join, relative_url)
-        category_urls.append(absolute_url)
-        
-    print(f"{len(category_urls)}件のカテゴリURLを取得しました。")
-    return category_urls
+def get_category_urls(html: str, base_url_for_join: str) -> list[str]:
+  """
+  受け取ったurlにアクセスし、そのページのHTMLを取得する
+  Args:
+    html (str): トップページのHTML
+    base_url_for_join (str): 相対パスを絶対パスに変換するためのベースurl
+  Returns:
+    str: 各カテゴリのurl
+  """
+  soup = BeautifulSoup(html, 'html.parser')
+  category_urls = []
+  # 'div.side_categoriesの中の、ulの中の、liの中の、aの中身' というCSSセレクタで目的のaタグをすべて取得
+  category_links = soup.select('div.side_categories ul li a')
+  
+  # 最初のリンクは 'Books' 全体なのでスキップ (スライス[1:]を使う)
+  for link in category_links[1:]:
+      # 相対パスを絶対パスに変換する
+      relative_url = link.get('href')
+      absolute_url = urljoin(base_url_for_join, relative_url)
+      category_urls.append(absolute_url)
+      
+  print(f"{len(category_urls)}件のカテゴリURLを取得しました。")
+  return category_urls
    
-def parse_books_info(html):
+def parse_books_info(html: str) -> list[dict]:
+  """
+  受け取ったhtmlの中から、本の情報を取得する
+  Args:
+    html (str): 各カテゴリのHTML
+  Returns:
+    list: 各カテゴリの書籍のタイトルと値段の辞書のリスト
+  """
   # response.text を Beautiful Soup で解析できるように変換
   soup = BeautifulSoup(html, 'html.parser')
   books_data = []
@@ -50,7 +74,14 @@ def parse_books_info(html):
     books_data.append({"title": title, "price": price})
   return books_data
 
-def write_data_by_csv(books_data):
+def write_data_by_csv(books_data: list[dict]) -> None:
+  """
+  書籍情報の辞書のリストを受け取り、CSVファイルに書き出す。
+  Args:
+    books_data (list[dict]): 全カテゴリの書籍のタイトルと値段の辞書のリスト
+  Returns:
+    None
+  """
   with open('books_data.csv', 'w', encoding='utf-8', newline="") as f:
     fieldnames = ['title', 'price']
     # 辞書を書き込むためのDictWriterを用意
@@ -61,7 +92,14 @@ def write_data_by_csv(books_data):
     writer.writerows(books_data)
     print("books_data.csv にデータを保存しました。")
 
-def main():
+def main() -> None:
+  """
+  プログラムのエントリーポイント。全体の処理を実行する。
+  Args:
+    None
+  Returns:
+    None
+  """
   # 練習サイトのURL
   #値の変わらない"定数"であれば、大文字表記する
   BASE_URL = "https://books.toscrape.com/index.html" # 例として練習サイトを使います
